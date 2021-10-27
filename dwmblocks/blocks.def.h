dafff0f21c21564b2bc7fccfcd0260bd264f668b
Erik Bravo <ebz.erik@gmail.com>
2021-10-26 21:41:45 -0500

    xwallpaper upload

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,8 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Vol:", "~/.scripts/pulse-volume.sh output",	30,		0},
+	{"Vol:", "~/.local/share/scripts/dwmblocks/pulse-volume.sh output",	30,		10},
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"", "~/.local/share/scripts/dwmblocks/battery_level.sh",	30,		0},
 	{"📅 ", "date '+%b %d (%a) %I:%M%p' | tr '[:lower:]' '[:upper:]'",		5,		0},
 };
 
